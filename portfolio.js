e6ebaec908917139d2fd96e89b27f568cc182747
akselfreeman215 <akselfreeman215@gmail.com>
2024-04-26 02:16:24 -0500

    Updated js and html

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,34 @@
-// main.js
+// portfolio.js
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
-const port = 5000;
+const port = 80;
 
+// Middleware to parse the body of the request
+app.use(express.urlencoded({ extended: true }));
+
+//serve html files without the extension
 app.use(express.static(path.join(__dirname, 'Portfolio'), {extensions: ['html']}))
-
-// Serve static files from the 'Portfolio' directory
-app.use(express.static(path.join(__dirname, 'Portfolio')));
 
 // Set up a route for the homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Portfolio', 'home.html'));
 });
 
+// Route to handle form submission
+app.post('/submit-form', (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(`Form submitted: Name - ${name}, Email - ${email}, Message - ${message}`);
+  res.redirect('/'); // Redirect back to the homepage
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Main server is running at http://localhost:${port}`);
+  console.log(`Main server is running at https://www.aksel.dev`);
 });
